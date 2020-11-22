54401383a98573345222e9fb0b81591e2218d214
Charlie <carlos.carlon17@tectijuana.edu.mx>
2020-11-21 19:18:30 -0800

    Practica

--- Unidad3/primero.s
+++ Unidad3/primero.s
@@ -1,11 +1,7 @@
-@ primero.s
-@   from thinkingeek.com
-@   http://thinkingeek.com/2013/01/09/arm-assembler-raspberry-pi-chapter-1/
-@   Defines a main function that returns 2 as an exit code.
-@	
+@primero.s
 
-	
+
 	.global main
-	
-main:   mov r0, #2       @ load immediate value 2 into Register r0 
-	bx lr            @ return 2 to Operating Sytem
+
+main:	mov r0, #4
+	bx lr
