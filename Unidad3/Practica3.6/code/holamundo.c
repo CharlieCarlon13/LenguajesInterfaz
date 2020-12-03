d8c6e2c6f05400e33499367ff7313ea0380f7a40
Carlos Armando Carlon Gerardo <61715659+CharlieCarlon13@users.noreply.github.com>
2020-12-02 17:15:28 -0800

    Create holamundo.c

---
+++ Unidad3/Practica3.6/code/holamundo.c
@@ -0,0 +1,14 @@
+// Instituto Tecnologico de Tijuana
+// Depto de Sistemas y Computación
+// Ing.En Sistemas Computacionales
+//
+// Autor : Carlos Armando Carlon Gerardo @nickname CharlieCarlon13
+// Repositorio: https://github.com/CharlieCarlon13/LenguajesInterfaz
+// Fecha de revisión : 02/12/2020
+
+#include <stdio.h>
+int main (void)
+{
+	printf("Hola bienvenidos a ARM64\n");
+	return 0;
+}
